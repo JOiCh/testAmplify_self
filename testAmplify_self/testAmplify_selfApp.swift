ed7c20b39f71bd460724cd58d81fc3d596fe4aa7
JOi Chao <ilieqreila@gmail.com>
2022-11-14 15:21:29 +0800

    Initial Commit

---
+++ testAmplify_self/testAmplify_selfApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAmplify_selfApp.swift
+//  testAmplify_self
+//
+//  Created by JOi Chao on 2022/11/14.
+//
+
+import SwiftUI
+
+@main
+struct testAmplify_selfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
